86053ae2f383a6a7af22b3f7af4ad07df8c78759
dantls <dan_tls@hotmail.com>
2020-04-04 14:37:47 -0400

    Completed back-end and front-end application

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import Notifications from '~/components/Notifications';
+import logo from '~/assets/logo-purple.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  const profile = useSelector((state) => state.user.profile);
+
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+        <aside>
+          <Notifications />
+
+          <Profile>
+            <div>
+              <strong>{profile.name}</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <img
+              src={
+                (profile.avatar && profile.avatar.url) ||
+                'https://api.adorable.io/avatars/285/abott@adorable.png'
+              }
+              alt=""
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
